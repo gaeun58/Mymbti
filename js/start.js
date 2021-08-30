96eaa79cc03fd8d44bd2292c0149e70a0b6b018c
gaeun58 <gaeun58@naver.com>
2021-08-30 23:33:56 +0900

    fadeIn, fadeOut 애니메이션 추가

--- js/start.js
+++ js/start.js
@@ -2,6 +2,10 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
 function begin(){
-    main.style.display = "none";
-    qna.style.display = "block";
+    main.style.WebkitAnimation = "fadeOut 1s";
+    main.style.animation = "fadeOut 1s";
+    qna.style.WebkitAnimation = "fadeIn 1s";
+    qna.style.animation = "fadeIn 1s";
+    //main.style.display = "none";
+    //qna.style.display = "block";
 }
